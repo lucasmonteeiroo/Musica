a46feecedb30dc43c21f670f80e9b6b42c578858
lucas augusto <luaugustomonteiror@gmail.com>
2026-09-04 20:41:57 -0300

    final

--- Program.cs
+++ Program.cs
@@ -2,6 +2,7 @@
 Console.WriteLine( @"Come Back - The Five Stairsteps");
 Console.ResetColor();
 Console.WriteLine("");
+Thread.Sleep(1000);
 
 Console.ForegroundColor = ConsoleColor.Blue;
 Console.WriteLine( @"Come back
@@ -9,6 +10,8 @@ Come back
 Come back to me");
 Console.ResetColor();
 Console.WriteLine("");
+Thread.Sleep(1000);
+
 
 Console.ForegroundColor = ConsoleColor.Blue;
 Console.WriteLine( @"Where have you gone now? (Ooh-ooh-ooh-ooh)
@@ -17,12 +20,16 @@ If you really love me (ooh-ooh-ooh-ooh)
 Let me be your foo");
 Console.ResetColor();
 Console.WriteLine("");
+Thread.Sleep(1000);
+
 
 Console.ForegroundColor = ConsoleColor.Yellow;
 Console.WriteLine( @"Just come back (come back) to me (come back)
 Come back (come back) to me (to me)");
 Console.ResetColor();
 Console.WriteLine("");
+Thread.Sleep(1000);
+
 
 Console.ForegroundColor = ConsoleColor.Blue;
 Console.WriteLine( @"What have I done (ooh-ooh-ooh-ooh)
@@ -31,12 +38,16 @@ If I could stop crying (ooh-ooh-ooh-ooh)
 I would have to say");
 Console.ResetColor();
 Console.WriteLine("");
+Thread.Sleep(1000);
+
 
 Console.ForegroundColor = ConsoleColor.Yellow;
 Console.WriteLine( @"Just come back (come back) to me (come back)
 Come back (come back) to me (to me)");
 Console.ResetColor();
 Console.WriteLine("");
+Thread.Sleep(1000);
+
 
 Console.ForegroundColor = ConsoleColor.Blue;
 Console.WriteLine( @"I'm so mad about you (mad about you)
@@ -45,9 +56,13 @@ You're always on my mind (ooh-ooh-ooh-ooh)
 I dream of you all the time");
 Console.ResetColor();
 Console.WriteLine("");
+Thread.Sleep(1000);
+
 
 Console.ForegroundColor = ConsoleColor.Yellow;
 Console.WriteLine( @"So, come back (come back) to me (come back)
 Come back (come back) to me (to me)");
 Console.ResetColor();
 Console.WriteLine("");
+Thread.Sleep(1000);
+
